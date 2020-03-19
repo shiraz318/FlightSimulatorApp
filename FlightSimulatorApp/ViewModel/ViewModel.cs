e9740f3f238bc781f9e302764c0286af4285213a
shiraz berger <shiraz422@gmail.com>
2020-03-19 08:35:52 +0200

    notes

--- FlightSimulatorApp/ViewModel/ViewModel.cs
+++ FlightSimulatorApp/ViewModel/ViewModel.cs
@@ -36,11 +36,11 @@ namespace FlightSimulatorApp.ViewModel
                 //"nili cohen"//
             }
         }
-        public double VM_Rudder { get { return model.Rudder; }}
-        public double VM_Elevator { get { return model.Elevator; } }
-        public double VM_Aileron { get { return model.Aileron; } }
-        public double VM_Throttle { get { return model.Throttle; } }
-        public double VM_Indicated_heading_deg { get { return model.Indicated_heading_deg; } set { vm_indicated_heading_deg = value; } }
+        public double VM_Rudder { get { return model.Rudder; }} // I think we should bind it to the text field of rudder (of the view) and to add a set that will do model.setSimulator(rudder, value) and we can delete the get and the propert of rudder in the model.
+        public double VM_Elevator { get { return model.Elevator; } }//kanal
+        public double VM_Aileron { get { return model.Aileron; } }//kanal
+        public double VM_Throttle { get { return model.Throttle; } }//kanal
+        public double VM_Indicated_heading_deg { get { return model.Indicated_heading_deg; } set { vm_indicated_heading_deg = value; } }//I think we can delete the set, and in the view we need to add to the event propertyChanged a function that checks the property name and change the appropriate progress bar accordingly OR we can do it by data binding - the progress bar will be bind to this property.
         public double VM_Gps_indicated_vertical_speed { get { return model.Gps_indicated_vertical_speed; } set { vm_gps_indicated_vertical_speed = value; } }
         public double VM_Gps_indicated_ground_speed_kt { get { return model.Gps_indicated_ground_speed_kt; } set { vm_gps_indicated_ground_speed_kt = value; } }
         public double VM_Airspeed_indicator_indicated_speed_kt { get { return model.Airspeed_indicator_indicated_speed_kt; } set { vm_airspeed_indicator_indicated_speed_kt = value; } }
